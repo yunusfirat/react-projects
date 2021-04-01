d1ae31c6638d924d8f97bb52e476ff07db0b5c86
yunusfirat <yunusfirat44@gmail.com>
2021-04-01 02:50:50 +0100

    first commit

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return <h2>reminder project setup
+    <h1>first commit</h1>
+  </h2>;
+
 }
 
 export default App;
